3c697723e9d703079c3bc9356257333f6c129118
arvinder004 <asdhoul004@gmail.com>
2025-10-09 15:57:29 +0530

    added env var

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.BASE_URL;
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const API = axios.create({
   baseURL: apiBaseUrl,
   headers: {
